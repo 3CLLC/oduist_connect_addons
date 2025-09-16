a81170334bc9ce32b9ed9a98bdda87f073bf0f74
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2025-09-16 16:51:09 +0000

    Fix computed field warnings - remove store=True and add consistent compute_sudo=True for recordings fields

--- connect/models/call.py
+++ connect/models/call.py
@@ -35,11 +35,11 @@ class Call(models.Model):
     recording_icon = fields.Html(compute='_get_recording_data', string='R')
     
     # Call-aggregated recording fields (only for recordings view)
-    recordings_count = fields.Integer(compute='_get_call_recordings_data', string='# Recordings', store=True)
-    recordings_total_duration = fields.Integer(compute='_get_call_recordings_data', string='Total Recording Duration') 
-    recordings_total_duration_human = fields.Char(compute='_get_call_recordings_data', string='Recording Duration')
-    recordings_widget = fields.Html(compute='_get_call_recordings_data', string='All Recordings', sanitize=False)
-    recordings_participants = fields.Char(compute='_get_call_recordings_data', string='Recorded Participants')
+    recordings_count = fields.Integer(compute='_get_call_recordings_data', compute_sudo=True, string='# Recordings')
+    recordings_total_duration = fields.Integer(compute='_get_call_recordings_data', compute_sudo=True, string='Total Recording Duration')
+    recordings_total_duration_human = fields.Char(compute='_get_call_recordings_data', compute_sudo=True, string='Recording Duration')
+    recordings_widget = fields.Html(compute='_get_call_recordings_data', compute_sudo=True, string='All Recordings', sanitize=False)
+    recordings_participants = fields.Char(compute='_get_call_recordings_data', compute_sudo=True, string='Recorded Participants')
     
     summary = fields.Html()
     called = fields.Char(readonly=True)
